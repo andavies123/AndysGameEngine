103498a54db22f73296111a68517f3ba8374106f
Andrew Davies <davies.andrew.123@gmail.com>
2023-09-14 14:54:08 -0700

    Added Vector3

---
+++ AndysGameEngine/Vector3.cs
@@ -0,0 +1,24 @@
+﻿namespace AndysGameEngine;
+
+public readonly struct Vector3
+{
+	public readonly float X;
+	public readonly float Y;
+	public readonly float Z;
+
+	public Vector3(float x, float y, float z)
+	{
+		X = x;
+		Y = y;
+		Z = z;
+	}
+
+	public static Vector3 operator +(Vector3 a, Vector3 b) =>
+		new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+	
+	public static Vector3 operator -(Vector3 a, Vector3 b) =>
+		new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+	
+	public static Vector3 operator *(Vector3 a, Vector3 b) =>
+		new(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+}
